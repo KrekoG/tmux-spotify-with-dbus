e51af7c9be69a66492d942900d5e08dd2aad2eb6
krekog <kreko.gergely@gmail.com>
2025-03-17 11:28:19 +0100

    Move menu to the middle of the screen

--- scripts/hub.sh
+++ scripts/hub.sh
@@ -2,7 +2,7 @@ scripts=$1
 media_player=$2
 action=$3
 
-$(tmux display-menu -T "#[align=centre fg=green]$media_player" -x R -y P \
+$(tmux display-menu -T "#[align=centre fg=green]$media_player" -x C -y C \
   "" \
   "Play/Pause"          p "run -b '$scripts/do_and_close.sh $scripts $media_player PlayPause'" \
   "Previous"            o "run -b '$scripts/do.sh $scripts $media_player Previous'" \
